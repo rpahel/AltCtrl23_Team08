9587764f8d58e695141e355031ca6275c16a98bd
rpahel <92730433+rpahel@users.noreply.github.com>
2023-10-19 10:46:55 +0200

    Google Drive Manager Prefab

--- Assets/Scripts/GoogleDrive/GoogleDriveManager.cs
+++ Assets/Scripts/GoogleDrive/GoogleDriveManager.cs
@@ -1,32 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
+using ScrollShop.CustomDebug;
+using ScrollShop.Enums;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityGoogleDrive;
 using UnityGoogleDrive.Data;
 
 public class GoogleDriveManager : MonoBehaviour
 {
-    public RenderTexture renderTex;
+    [SerializeField]
+    private RenderTexture _renderTexture;
+    [SerializeField]
+    private Camera _screenshotCamera;
     
+    //==== Public Methods ====
     public void TakeScreenShot()
     {
-        RenderTexture.active = renderTex;
-        Texture2D destinationTexture = new Texture2D(renderTex.width, renderTex.height, TextureFormat.RGB24, false);
-        Rect regionToReadFrom = new Rect(0, 0, renderTex.width, renderTex.height);
+        RenderTexture mRt = new RenderTexture(
+            _renderTexture.width, 
+            _renderTexture.height, 
+            _renderTexture.depth, 
+            RenderTextureFormat.ARGB32, 
+            RenderTextureReadWrite.sRGB);
+        
+        mRt.antiAliasing = _renderTexture.antiAliasing;
+
+        _screenshotCamera.targetTexture = mRt;
+        _screenshotCamera.Render();
+        RenderTexture.active = mRt;
+        
+        Texture2D destinationTexture = new Texture2D(mRt.width, mRt.height, TextureFormat.ARGB32, false);
+        Rect regionToReadFrom = new Rect(0, 0, _renderTexture.width, _renderTexture.height);
         destinationTexture.ReadPixels(regionToReadFrom, 0, 0);
         destinationTexture.Apply();
+        
         StartCoroutine(UploadImageCoroutine(destinationTexture));
     }
 
+    //==== Private Methods ====
     private IEnumerator UploadImageCoroutine(Texture2D image)
     {
         byte[] png = image.EncodeToJPG();
         File file = new File{Name = "Test upload image", Content = png};
         var request = GoogleDriveFiles.Create(file);
-        request.Fields = new List<string> {"id"};
+        request.Fields = new List<string> {"id", "name"};
         yield return request.Send();
-        print(request.IsError);
-        print(request.ResponseData.Content);
-        print(request.ResponseData.Id);
+
+        if (!DebugConsole.Instance)
+            yield break;
+        
+        if (request.IsError)
+        {
+            DebugConsole.Instance.Print("GDrive : Error !", LOGTYPE.ERROR);
+            DebugConsole.Instance.Print("GDrive : " + request.ResponseData.Content, LOGTYPE.ERROR);
+        }
+        else
+        {
+            DebugConsole.Instance.Print("GDrive : Image upload successful.");
+            DebugConsole.Instance.Print("GDrive : Image ID = " + request.ResponseData.Id);
+            DebugConsole.Instance.Print("GDrive : Image Name = " + request.ResponseData.Name);
+        }
     }
 }
