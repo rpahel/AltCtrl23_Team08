4d3fe71279c9ad9528c6989983e65f7a613d9eb4
Cryp3rone <Manach.Nicolas.mn@gmail.com>
2023-10-19 09:51:12 +0200

    Websocket client

--- Assets/Scripts/AI/AiManager.cs
+++ Assets/Scripts/AI/AiManager.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using WebSocketSharp;
 
 namespace ScrollShop.AI
 {
@@ -9,18 +10,34 @@ namespace ScrollShop.AI
         [SerializeField] private AiManagerProxy _proxy;
         [SerializeField] private Bridge _bridgeSO;
         public Bridge GetBridge => _bridgeSO;
+        public string port = "8080";
+        private WebSocket ws;
 
         //== Private Methods =============================
         private void Awake()
         {
             if(_proxy)
                 _proxy.Manager = this;
+
+            ResetWebSocket();
         }
 
         private void Start()
         {
             if(_bridgeSO)
                 _bridgeSO.Initialize();
+        }
+
+        private void Update()
+        {
+            if (ws == null)
+            {
+                return;
+            }
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                ResetWebSocket();
+            }
         }
 
         private void OnDestroy()
@@ -30,6 +47,33 @@ namespace ScrollShop.AI
 
             if(_proxy && _proxy.Manager == this)
                 _proxy.Manager = null;
+        }
+
+        private void ResetWebSocket()
+        {
+            if (ws != null)
+            {
+                ws.Close();
+                ws = null;
+            }
+
+            ws = new WebSocket("ws://localhost:" + port);
+            ws.Connect();
+            ws.OnMessage += (sender, e) =>
+            {
+                Debug.Log("Message Received from " + ((WebSocket)sender).Url + ", Data : " + e.Data);
+                string playerType = e.Data.Split("$")[0];
+                string poseName = e.Data.Split("$")[1];
+                string confidence = e.Data.Split("$")[2];
+
+                int playerIndex = -1;
+                if (playerType.Equals("Spell"))
+                    playerIndex = 0;
+                else if (playerType.Equals("Type"))
+                    playerIndex = 1;
+
+                _bridgeSO.SetPose(playerIndex, poseName);
+            };
         }
     }
 }
